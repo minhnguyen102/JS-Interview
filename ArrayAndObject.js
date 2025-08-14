054c2673e11d0d226745620d4628a31842c45772
Minh Nguyen <minhkhac1002@gmail.com>
2025-08-14 15:11:55 +0700

    Obejct

---
+++ ArrayAndObject.js
@@ -0,0 +1,88 @@
+// Bai 1
+    // const arr = [1, 2, 3];
+    // arr[10] = 99;
+    // console.log(arr.length); // 11 length không phải số lượng phần tử thật mà chỉ là chỉ số lớn nhất + 1
+    // console.log(arr[5]);  // undefined
+
+/**
+ * arr[10] => tự động mở rộng mảng đến chi số 10
+ * arr[3] -> arr[9]: undefined (empty slots)
+ */
+
+
+// Bai 2
+    // console.log([1, 2] == [1, 2]); // false
+    // // console.log([1, 2] === [1, 2]); // false
+
+    // const a = [1, 2];
+    // const b = a; // b và a cùng tham chiếu đến một địa chỉ 
+    // console.log(a === b); // true
+
+/**
+ * So sánh địa chỉ tham chiếu đến mảng chứ không so sánh nội dung mảng 
+ */
+
+// Bai 3
+    // const arr1 = [1, 2];
+    // const arr2 = arr1;
+    // const arr3 = [...arr1];
+
+    // arr2.push(3); 
+    // arr3.push(4);
+
+    // console.log(arr1); // [1 2 3]
+    // console.log(arr2); // [1 2 3]
+    // console.log(arr3); // [1 2 4]
+
+/**
+ * mảng 1 và 2 cùng tham chiếu đến 1 địa chỉ => 1 thay đổi thì 2 cũng thay đổi
+ * mảng 3 chỉ giải tham chiếu (lấy nội dung của mảng một), mảng 3 có địa chỉ tham chiếu khác hoàn toàn với mảng 1 2. 
+ * Mảng 3 thay đổi không làm thay đổi nội dung mảng 1 2
+ */
+
+// Bai 4
+    // const obj = {};
+    // obj[1] = "one"; // tự động ép key 1 từ dạng number về string
+    // obj["1"] = "ONE"; // ghi đè lên key "1" one -> ONE
+
+    // console.log(obj[1]); // ONE
+    // console.log(Object.keys(obj));  "1"
+
+/**
+ * Key trong object luôn là string 
+ */
+
+// Bài 5
+    // const user = { profile: { name: "Alice" } };
+    // console.log(user.profile?.name); // Alice
+    // console.log(user.account?.balance); // undefined
+
+// Bài 6
+    // const person = { name: "Bob", age: 25, job: null };
+    // const { name: n, age: a, job = "Developer" } = person;
+    // console.log(n, a, job); // Bob, 25, null
+
+/**
+ * Nếu lấy giá trị không có trong objject => nhận giá trị khởi tạo làm giá trị
+ * nhưng nếu giá trị trong object là null => nhân null làm giá trị chứ không lấy giá trị khởi tạo
+ */
+
+// Bài 7 
+    // const user = {
+    //   name: "Alice",
+    //   age: 25,
+    //   password: "123456"
+    // };
+
+    // function removePassword(obj) {
+    //   // Viết code trả về object mới không có key 'password'
+    //     const {password, ...newData} = user
+    //     return newData
+    // }
+
+    // console.log(removePassword(user));
+    // console.log(user)
+
+/**
+ * Không nên xóa trực tiếp bằng cách delete obj.password => xóa luôn trên data gốc
+ */
